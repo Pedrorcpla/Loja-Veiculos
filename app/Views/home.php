606b180a286b378e9936a0913573d3040bfc92dc
Pedrorpcla <pedrorcpla@gmail.com>
2022-03-16 14:56:24 -0300

    Adicionado telas de contato, sobre nós e login

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,47 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loja de Veículos</title>
-    <link href='<?php base_url() ?>/css/style.css' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/71d37f4069.js" crossorigin="anonymous"></script>
-    <style>
-        .modal-fullscreen{
-            width: 25vw;
-            float: right;
-        }
-    </style>
-</head>
-<body class='bg-light'>
-    <div class="row bg-white border-bottom border-danger border-3" id='header'>
-        <div class="col-5 offset-1">
-            <h3>Loja de Veículos</h3>
-        </div>
-        <div class="col-1 offset-4 text-danger">
-            <button  type="button" class="btn border-danger border-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Menu</button>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen">
-        <div class="modal-content">
-        <div class="modal-header border-bottom border-danger border-3">
-            <h5 class="modal-title" id="exampleModalLabel">Menu</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <a class='nav' href="home">Home</a>
-            <a class='nav' href="sobre">Quem Somos</a>
-            <a class='nav' href="contato">Contato</a>
-        </div>
-        </div>
-    </div>
-    </div>
-    
     <div class="row" id='corpo'>
         <div class="col-8 offset-2 bg-white">
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
@@ -66,10 +22,4 @@
                 </button>
             </div>
         </div>
-
-
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
---
+++ app/Views/login.php
@@ -0,0 +1,17 @@
+    <div class="row" id='corpo'>
+        <div class="col-8 offset-2 bg-white" align='center'>
+            <br>
+            <div class="col-10 border-bottom border-warning">
+                <h1>Cadastro de Veículos</h1>
+            </div>
+            <br>
+            <div class="col-10 contato">
+                <input type="text" class='form-control' placeholder='Marca do carro bionico...'>
+                <input type="text" class='form-control' placeholder='Modelo do carro fantástico...'>
+                <input type="text" class='form-control' placeholder='Placa do carro simpático...'>
+                <input type="text" class='form-control' placeholder='Cor do carro titânico...'>
+            
+                <button class='btn btn-danger col-10'>Enviar</button>
+            </div>
+        </div>
+    </div>
